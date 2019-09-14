c7b1def3a3b2e602c906e2db565b9453011f26e5
Maja Jensen <eaamnje@students.eaaa.dk>
2019-09-14 22:56:59 +0200

    Valgbokse og kommentarer i JS

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+// Funktion som indsætter Thores content når siden loader
 window.onload = function () {
   thoreFunction();
 
+
 }
 
-
+// Funktion som kun viser header indtil der klikkes på linket, hvorefter main ændres vra none til block
 function velkommen() {
 
   document.getElementById("header").style.display = "none";
@@ -11,6 +13,7 @@ function velkommen() {
 
 }
 
+// Funktion som sætter stemningsvideoen på pause, når den ikke er i billedet
 scrollStemning = function () {
   if (document.getElementById("stemningsvideo").getBoundingClientRect().bottom < 350 || document.getElementById("stemningsvideo").getBoundingClientRect().top > 100)
     document.getElementById("stemningsvideo").pause();
@@ -19,7 +22,7 @@ scrollStemning = function () {
   document.getElementById("stemningsvideo").loop = true;
 }
 
-
+// Funktion som pauser Thores interview video, når videoen er udenfor vinduet
 scrollThore = function () {
   if (document.getElementById("interviewThore").getBoundingClientRect().bottom < -20 || document.getElementById("interviewThore").getBoundingClientRect().top > 850)
     document.getElementById("interviewThore").pause();
@@ -28,6 +31,7 @@ scrollThore = function () {
   document.getElementById("interviewThore").loop = true;
 }
 
+// Funktion som pauser Franciscas interview video, når videoen er udenfor vinduet
 scrollFran = function () {
   if (document.getElementById("interviewFran").getBoundingClientRect().bottom < -20 || document.getElementById("interviewFran").getBoundingClientRect().top > 850)
     document.getElementById("interviewFran").pause();
@@ -37,52 +41,66 @@ scrollFran = function () {
 }
 
 
-
+// Funktion som starter andre scroll funktioner, som gør at videoerne starter
 window.onscroll = function () {
   scrollStemning();
   scrollThore();
   scrollFran();
 
 }
-
-function interviewTime() {
-  var curTime = document.getElementById("interviewThore").currentTime;
-
-  curTime = x.value;
-  document.getElementById("interviewThore").addEventListener("mouseover", playInterviewThore);
-}
+ 
 
 
 //THORE INTERVIEW
 {
+  // Variabel som gemmer tiden på videoen når musen fjernes fra interviewet
   var videoTimerThore = 0;
 
-
+// Funktion som sker når man hover over Thores video
   function playInterviewThore() {
+    // Frans video udskiftes til ventevideoen
     document.getElementById("interviewFran").src = "video/vente-francisca.mp4";
+    // Sætter Frans ventevideo til at loope
     document.getElementById("interviewFran").loop = true;
+    // Sæt Frans ventevideo til at spille
     document.getElementById("interviewFran").play();
 
+    // Ændre Thores video til interview videoen
     document.getElementById("interviewThore").src = "video/interview_thore.mp4";
+    // Brug tiden, som blev gemt i variablen videoTimerThore
     document.getElementById("interviewThore").currentTime = videoTimerThore;
+    // Fjern loop fra interview videoen
     document.getElementById("interviewThore").loop = false;
+    // Sæt Thores video til at afspilde
     document.getElementById("interviewThore").play();
 
 
   }
 
+  // Funktion som sker når musen fjernes fra Thores video
   function pauseInterviewThore() {
+
+    //Inden videoen skiftes så tildeler vi variablen videoTimerThore værdien for videoens tidspunkt
+    // Denne variabel bruger vi når musen kommer ind over igen
     videoTimerThore = document.getElementById("interviewThore").currentTime;
+    // Så spørger vi om videoens gemte tidspunkt er større ind eller lig med længden af filmen
     if (videoTimerThore >= document.getElementById("interviewThore").duration) {
+      // Hvis ja, så sætter vi tiden til 0, så den starter forfra næste gang
       videoTimerThore = 0;
     }
 
+    // Ændre Frans video tilbage til vælg mig fra vente
     document.getElementById("interviewFran").src = "video/vaelg-mig-francisca-kort.mp4";
+    // Sætte Frans vælg mig video til at loope
     document.getElementById("interviewFran").loop = true;
+    // Sætte Frans vælg mig video til at afspille
     document.getElementById("interviewFran").play();
 
+// Ændre Thores video tilbage til vælg mig videoen fra interviewet
     document.getElementById("interviewThore").src = "video/vaelg-mig-thore-kort.mp4";
+// Sætte Thores vælg mig video til at loope
     document.getElementById("interviewThore").loop = true;
+    // Sætte Thores vælg mig video til at loope
     document.getElementById("interviewThore").play();
 
 
@@ -91,6 +109,7 @@ function interviewTime() {
 
 
 //FRAN INTERVIEW
+// Læs kommentarer for Thores interview!
 {
   var videoTimerFran = 0;
 
@@ -123,7 +142,7 @@ function interviewTime() {
   }
 }
 
-// SKift af indhold ved klik på person
+// SKift af indhold ved klik på person (Thore)
 
 function thoreFunction() {
   // Ændre teksten i "sig ja" paragraphen
@@ -136,7 +155,15 @@ function thoreFunction() {
   document.getElementById("valgfag1").style.backgroundImage = "url(img/valgfag_analog.jpg)";
   // Skifte andet valgfagsbillede
   document.getElementById("valgfag2").style.backgroundImage = "url(img/valgfag_elektronisk_musik.png)";
+
+  // Give vælg mig videoen en ramme (outline) når den er valgt (ved klik)
+  document.getElementById("interviewThore").style.outline = "solid 1.5em #ffa50099";
+  // Fjerne rammen fra Frans video
+  document.getElementById("interviewFran").style.outline = "none";
 }
+
+// SKift af indhold ved klik på person (Francisca)
+// Se kommentarer for Thore
 
 function franFunction() {
   document.getElementById("tekstJa").innerHTML = "Jeg Ikke ja til noget";
@@ -144,6 +171,9 @@ function franFunction() {
   document.getElementById('myaudio').src = 'audio/eriksminde_lyd.mp3';
   document.getElementById("valgfag1").style.backgroundImage = "url(img/valgfag_sts.png)";
   document.getElementById("valgfag2").style.backgroundImage = "url(img/valgfag_sts.png)";
+
+  document.getElementById("interviewThore").style.outline = "none";
+  document.getElementById("interviewFran").style.outline = "solid 1.5em #ffa50099";
 }
 
 // Lyde til valgfag
